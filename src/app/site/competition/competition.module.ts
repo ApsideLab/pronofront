373eff692c97a7f8133b9ecc6573e82efb875bad
Sebriniel <meliss.leal@gmail.com>
2019-10-22 13:24:07 +0200

    [TRELLO-89][FEATURE] Faire la route de la page du détail d’un concours + création du composant + service

--- src/app/site/competition/competition.module.ts
+++ src/app/site/competition/competition.module.ts
@@ -5,6 +5,9 @@ import { AuthGuard } from 'src/app/services/auth/auth.guard';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 
+import { CompetitionListComponent } from '../competition-list/competition-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 const routes: Routes = [
   { path: 'competitions', canActivate: [AuthGuard], component: CompetitionComponent }
 ];
@@ -13,8 +16,14 @@ const routes: Routes = [
   declarations: [CompetitionComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([
+      {path: 'competitions/:id', component: CompetitionComponent},
+      // {path: 'actorsUpdate', redirectTo:'/actors'},
+      {path:'competitions', component: CompetitionListComponent}
+      ])
   ]
 })
 export class CompetitionModule { }
