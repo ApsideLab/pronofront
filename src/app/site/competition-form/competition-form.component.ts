eed3f552dabc67ec07159d40b9abad0dd4d01709
Sebriniel <meliss.leal@gmail.com>
2019-10-23 15:24:43 +0200

    [TRELLO-94][FEATURE] Gestion des erreurs

--- src/app/site/competition-form/competition-form.component.ts
+++ src/app/site/competition-form/competition-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Competition } from '../../Models/competition'
 import { CompetitionService } from '../../services/competition/competition.service'
 import {Location} from '@angular/common';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { FormGroup,  FormBuilder,  Validators, FormControl } from '@angular/forms';
 import {DateValidator} from '../../helpers/date.validator';
 
 @Component({
@@ -14,13 +14,33 @@ import {DateValidator} from '../../helpers/date.validator';
 export class CompetitionFormComponent {
 
   competition: Competition;
+  competitionForm: FormGroup;
+  error: any;
 
   constructor(private competitionService: CompetitionService, private location: Location, private fb: FormBuilder) { 
     this.competition = new Competition();
+    this.competitionForm = this.fb.group({
+      label: new FormControl(),
+      startDate: new FormControl(),
+      endDate: new FormControl(),
+    });
+    
+  }
+
+  private bindToModel(): void {
+    this.competition.label = this.competitionForm.get('label').value;
+    this.competition.startDate = this.competitionForm.get('startDate').value;
+    this.competition.endDate = this.competitionForm.get('endDate').value;
   }
 
   submit() {
-    this.competitionService.save(this.competition).subscribe(result => this.competitionService.goToContestList())
+    this.bindToModel();
+    // this.competitionService.save(this.competition).subscribe(result => this.competitionService.goToContestList())
+    this.competitionService.save(this.competition).subscribe({
+      error: err => {console.error(err.error.message);
+                      this.error = err.error.message},
+      complete: () => this.competitionService.goToContestList()
+    })
   }
 
   cancel() {
