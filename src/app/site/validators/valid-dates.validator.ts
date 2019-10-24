c645db9f4c9150f1358babfcb1f086aa8f34aa05
Sebriniel <meliss.leal@gmail.com>
2019-10-24 10:13:45 +0200

    [TRELLO-77][REFACTO]

--- src/app/site/validators/valid-dates.validator.ts
+++ src/app/site/validators/valid-dates.validator.ts
@@ -8,20 +8,15 @@ export function validDates(controlStartDate: string, controlEndDate: string) {
         if(controlSD.value == null || controlED.value == null) {
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
-            console.log("SD ou ED = null")
         } else if (controlSD.value > controlED.value || controlSD.value == controlED.value) {
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
-            console.log("SD >= ED")
         } else if (controlSD.value.length != 16 || controlED.value.length != 16) {
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
-            console.log("!= 16")
         } else {
-            console.log("good")
             const sdYear = controlSD.value.slice(0, controlSD.value.indexOf("-"));
             const edYear = controlED.value.slice(0, controlED.value.indexOf("-"));
-            console.log(sdYear, edYear)
             if(sdYear < 1900 || edYear < 1900) {
                 controlSD.setErrors({validDates: true});
                 controlED.setErrors({validDates: true});
