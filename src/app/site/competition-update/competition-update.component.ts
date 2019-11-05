242cf5737709e0288ce4ab073e9693f7b3b6648d
Sebriniel <meliss.leal@gmail.com>
2019-11-05 11:46:03 +0100

    [TRELLO-105/106/107/108][FEATURE] Template + contraintes formulaire + bouton annuler

--- src/app/site/competition-update/competition-update.component.ts
+++ src/app/site/competition-update/competition-update.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CompetitionService } from 'src/app/services/competition/competition.service';
 import { Competition } from 'src/app/Models/competition';
 import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { validDates } from '../validators/valid-dates.validator';
 
 @Component({
   selector: 'app-competition-update',
@@ -11,9 +14,27 @@ import { ActivatedRoute } from '@angular/router';
 export class CompetitionUpdateComponent implements OnInit {
   id : number;
   contest: Competition;
+  fixedStartDate: String;
+  fixedEndDate: String;
+  competitionForm: FormGroup;
+  error: any;
 
-  constructor(private competitionService: CompetitionService, private route: ActivatedRoute) { 
+  constructor(private competitionService: CompetitionService, private location: Location, private route: ActivatedRoute, private fb: FormBuilder) { 
     this.contest = new Competition();
+    this.contest = new Competition();
+    this.competitionForm = this.fb.group({
+      label: new FormControl(),
+      startDate: new FormControl(),
+      endDate: new FormControl(),
+    }, {
+      validator: validDates('startDate', 'endDate')
+    });
+  }
+
+  private bindToModel(): void {
+    this.contest.label = this.competitionForm.get('label').value;
+    this.contest.startDate = this.competitionForm.get('startDate').value;
+    this.contest.endDate = this.competitionForm.get('endDate').value;
   }
 
   ngOnInit() {
@@ -21,8 +42,27 @@ export class CompetitionUpdateComponent implements OnInit {
       this.id = +params['id'];
       this.competitionService.get(this.id).subscribe(data =>{
         this.contest = data;
+        this.fixedStartDate = this.contest.startDate.toString().replace(/ /g, "T").slice(0,16);
+        this.fixedEndDate = this.contest.endDate.toString().replace(/ /g, "T").slice(0,16);
+        this.competitionForm.get('label').setValue(this.contest.label);
+        this.competitionForm.get('startDate').setValue(this.fixedStartDate);
+        this.competitionForm.get('endDate').setValue(this.fixedEndDate);
       });
     });
+    console.log(this.competitionForm);
+  }
+
+  submit() {
+    this.bindToModel();
+    // this.competitionService.save(this.competition).subscribe(result => this.competitionService.goToContestList())
+    this.competitionService.update(this.contest).subscribe({
+      error: err => this.error = err.error.message,
+      complete: () => this.competitionService.goToContestList()
+    })
+  }
+
+  cancel() {
+    this.location.back();
   }
 
 }
--- src/app/site/validators/valid-dates.validator.ts
+++ src/app/site/validators/valid-dates.validator.ts
@@ -4,23 +4,30 @@ export function validDates(controlStartDate: string, controlEndDate: string) {
     return (formGroup: FormGroup) => {
         const controlSD = formGroup.controls[controlStartDate];
         const controlED = formGroup.controls[controlEndDate];
+        console.log(controlSD.value, controlED.value);
 
         if(controlSD.value == null || controlED.value == null) {
+            console.log("controlSD.value == null || controlED.value == null")
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
         } else if (controlSD.value > controlED.value || controlSD.value == controlED.value) {
+            console.log("controlSD.value > controlED.value || controlSD.value == controlED.value")
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
         } else if (controlSD.value.length != 16 || controlED.value.length != 16) {
+            console.log("controlSD.value.length != 16 || controlED.value.length != 16")
+            console.log(controlSD.value, controlED.value);
             controlSD.setErrors({validDates: true});
             controlED.setErrors({validDates: true});
         } else {
             const sdYear = controlSD.value.slice(0, controlSD.value.indexOf("-"));
             const edYear = controlED.value.slice(0, controlED.value.indexOf("-"));
             if(sdYear < 1900 || edYear < 1900) {
+                console.log("sdYear < 1900 || edYear < 1900")
                 controlSD.setErrors({validDates: true});
                 controlED.setErrors({validDates: true});
             } else {
+                console.log("else")
                 controlSD.setErrors(null);
                 controlED.setErrors(null);
             }
